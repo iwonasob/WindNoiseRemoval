333a962951bdf39dbf8b341140b41cc2ff11d1a2
Iwona <i.sobieraj@surrey.ac.uk>
2016-11-23 14:39:36 +0100

    Refactored pre_processing

--- pre_processing.py
+++ pre_processing.py
@@ -8,53 +8,55 @@ from scipy.io.wavfile import *
 
 def pre_processing(filename_speech, filename_wind, SNRin):
 
-	######### PRE-PROCESSING: ########################
+    ######### PRE-PROCESSING: ########################
 
-	# usage:
-	# filename_wind = "data/test_raw/wind/wind2.wav"
-	# filename_speech = "data/test_raw/female1.wav"
-	# SNRin = 0 # input SNR
-	# (y,x,wn) = pre_processing(filename_speech, filename_wind, SNRin)
+    # usage:
+    # filename_wind = "data/test_raw/wind/wind2.wav"
+    # filename_speech = "data/test_raw/female1.wav"
+    # SNRin = 0 # input SNR
+    # (y,x,wn) = pre_processing(filename_speech, filename_wind, SNRin)
 
-	# load files
-	[sr_wn, wn]=scipy.io.wavfile.read(filename_wind)
-	[sr_x, x]=scipy.io.wavfile.read(filename_speech)
+    # load files
+    [sr_wn, wn]=scipy.io.wavfile.read(filename_wind)
+    [sr_x, x]=scipy.io.wavfile.read(filename_speech)
 
-	# Normalize
-	x = x/np.max(abs(x));
-	wn = wn/np.max(abs(wn));
+    # Normalize
+    x = x/np.max(abs(x));
+    wn = wn/np.max(abs(wn));
 
-	# Crop signals so that they have same size
-	Lx = x.size
-	Lwn = wn.size
-	L = min(Lx,Lwn)
+    # Crop signals so that they have same size
+    Lx = x.size
+    Lwn = wn.size
+    L = min(Lx,Lwn)
 
-	wn = wn[0:L] # crop signal
-	x = x[0:L]
+    wn = wn[0:L] # crop signal
+    x = x[0:L]
 
-	# Normalize wind to desired SNR:
-	wn = wn * np.sqrt(np.sum(x**2)/(np.sum(wn**2)*(10**(SNRin/10))))
+    # Normalize wind to desired SNR:
+    wn = wn * np.sqrt(np.sum(x**2)/(np.sum(wn**2)*(10**(SNRin/10))))
 
-	# Create mixture:
-	y = x + wn # Noisy signal
+    # Create mixture:
+    y = x + wn # Noisy signal
 
-	return(x, y, sr_x)
+    return(x, y, sr_x)
 
-	# # Outputs:
-	# y: mixture
-	# x: clean speech
-	# wn: wind
+    # # Outputs:
+    # y: mixture
+    # x: clean speech
+    # wn: wind
 
-def prepare_training_data():
-	filename_wind_train = os.path.join('data', 'train', 'wind_train_1min.wav')
 
-	## audio parameters
-	n_fft = 512
-	hop_size = 128
-	gamma = 2
+def prepare_data(signal):
+    ## input: time signal
+    ## output: normalized magnitude, phase, number of fft bins, hop size and gamma parameter (power of magnitude)
 
-	[sr, w_train ] = read(filename_wind_train)
-	wind_stft = librosa.core.stft(w_train, n_fft, hop_size)
-	magnitude_wind=np.abs(wind_stft)**gamma
-	magnitude_wind_norm=magnitude_wind/np.max(magnitude_wind)
-	return magnitude_wind_norm, sr, n_fft, hop_size, gamma
+    ## audio parameters
+    n_fft = 512
+    hop_size = 128
+    gamma = 2
+
+    stft = librosa.core.stft(signal, n_fft, hop_size)
+    [m, phase] =librosa.core.magphase(stft)
+    magnitude=np.abs(stft)**gamma
+    magnitude_norm=magnitude/np.max(magnitude)
+    return magnitude_norm, phase, n_fft, hop_size, gamma
