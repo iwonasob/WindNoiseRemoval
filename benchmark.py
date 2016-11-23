46813102dd806379196471c79a615b157788f2cb
Cristóvão Cruz <cristovao@noiselessimaging.com>
2016-11-23 13:34:04 +0200

    Benchmark: add -20dB, comment function

--- benchmark.py
+++ benchmark.py
@@ -14,7 +14,39 @@ import scipy.io.wavfile
 
 #def benchmark(f, dataset, of):
 def benchmark(f, dataset, generate_wav=False):
+    """Benchmark method f on a dataset.
     
+    f is a function handle to your method, for example:
+    Simple case, no extra parameters:
+        f = lambda x, x_fs: fantastic_wind_remover(x, x_fs)
+    Second case, extra parameters:
+        f = lambda x, x_fs: amazing_wind_remover(x, x_fs, fixed_param1, fixed_param2)
+    
+    dataset is a number between 0 and 7:
+        0: wind1, SNR =   0dB
+        1: wind1, SNR =  -5dB
+        2: wind1, SNR = -10dB
+        3: wind1, SNR = -20dB
+        4: wind2, SNR =   0dB
+        5: wind2, SNR =  -5dB
+        6: wind2, SNR = -10dB
+        7: wind2, SNR = -20dB
+    If generate_wav is True, wav files will be generated, with both the input and the results.
+    
+    Each dataset is comprised of 6 speech samples, 3 males and 3 females, all corrupted with the same wind.
+    
+    This function outputs 5 things:
+        q: method gain (dB) (q_output - q_input)
+        y: list with the results for each speech sample
+        y_fs: sampling rate of the results
+        q_input: input signal SNR (compared to the ground truth)
+        q_output: result SNR (compared to the ground truth)
+    
+    Just print(q) to show how good your method is. You can mostly ignore the other outputs.
+    
+    Check run_benchmark for an example.
+    """
+
     of = lambda x_true, x: compute_snr(x_true, x)
     
     wind_f_stem = os.path.join('data', 'test_raw', 'wind')
@@ -34,13 +66,19 @@ def benchmark(f, dataset, generate_wav=False):
         snr_in = -10
         wind_f = 'wind1'
     elif dataset == 3:
+        snr_in = -20
+        wind_f = 'wind1'
+    elif dataset == 4:
         snr_in = 0
         wind_f = 'wind2'
-    elif dataset == 4:
+    elif dataset == 5:
         snr_in = -5
         wind_f = 'wind2'
-    elif dataset == 5:
+    elif dataset == 6:
         snr_in = -10
+        wind_f = 'wind2'
+    elif dataset == 7:
+        snr_in = -20
         wind_f = 'wind2'
         
     fn = len(voice_f)
